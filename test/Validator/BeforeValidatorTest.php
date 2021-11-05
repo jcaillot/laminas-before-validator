adbfd555d2abdde0e0b11356fdc74abdfa9b067b
jerome caillot <jerome@chaman.ca>
2021-11-05 15:54:08 -0400

    :test_tube: updating tests

--- test/Validator/BeforeValidatorTest.php
+++ test/Validator/BeforeValidatorTest.php
@@ -11,7 +11,24 @@ use Chaman\Validator\BeforeValidator;
 // php vendor/bin/phpunit test/Validator/BeforeValidatorTest.php
 class BeforeValidatorTest extends TestCase
 {
-    public function testStrictOption()
+
+
+    public function dataProviderInvalidUsingStrict()
+    {
+        return [
+            ['2012/08/01', ['date2' => '2012/08/01']],
+            ['2012/08/19', ['date2' => '2012/08/03']],
+            ['2021/09/12', ['date2' => '2019/08/23']],
+            ['2021/10/12', ['date2' => '2021/10/12']],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderInvalidUsingStrict
+     * @param string $date
+     * @param array $context
+     */
+    public function testInvalidUsingStrictOption(string $date, array $context)
     {
         $options = [
             'second_date_field' => 'date2',
@@ -19,13 +36,13 @@ class BeforeValidatorTest extends TestCase
             'strict' => true,
         ];
         $validator = new BeforeValidator($options);
-        $value = '2012/08/01';
-        $context = ['date2' => '2012/08/01'];
-
-        $isValid = $validator->isvalid($value, $context);
+        $isValid = $validator->isvalid($date, $context);
         $this->assertFalse($isValid);
+    }
 
 
+    public function testValidUsingStrictOption()
+    {
         $options = [
             'second_date_field' => 'date2',
             'date_format' => 'Y/m/d',
